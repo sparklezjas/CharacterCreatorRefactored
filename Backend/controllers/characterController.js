a4e6d0a25597fb32fc4bad26d5f61fec0c32ef18
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-08-02 16:17:20 -0500

    cleaning up backend

--- Backend/controllers/characterController.js
+++ Backend/controllers/characterController.js
@@ -4,35 +4,34 @@ const mongoose = require ('mongoose')
 
 const getCharacters = async (req, res) => {
 
-    const userID = req.user._id 
-
+const userID = req.user._id 
       try {
     const characters = await Characters.find({ user_id: userID }).sort({createdAt: -1})
 
     res.status(200).json(characters)
-} catch (error) {
-  res.status(500).json({ error: 'Error fetching characters'})
+    } catch (error) {
+    res.status(500).json({ error: 'Error fetching characters'})
 }}
 
 const getCharacter = async (req, res) => {
     const {id} = req.params
-    if (!mongoose.Types.ObjectId.isValid(id)){
+      if (!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({error: 'No such character'})
     }
 
     const character = await Characters.findById(id)
-    if (!character){
+      if (!character){
         return res.status(404).json({error: "No such character"})
     }
-    res.status(200).json(character)
+      res.status(200).json(character)
 }
 
 const createCharacter = async (req, res) => {
     const {head, face, body, arms, legs, weapon, characterName, HP} = req.body
-    if (!validator.isLength(characterName, { min: 1 })) {
+      if (!validator.isLength(characterName, { min: 1 })) {
         return res.status(400).json({ error: 'Character name is required' });
-      }
-    try{ 
+    }
+      try{ 
         const character = await Characters.create({head, face, body, arms, legs, weapon, characterName, HP, user_id: req.user._id})
         res.status(200).json(character)
     }
@@ -55,27 +54,21 @@ const deleteCharacter = async (req, res) => {
 
 const updateCharacter = async (req, res) => {
     const { id } = req.params;
-  
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({ error: 'No such character' });
       }
-    
-      const { characterName } = req.body;
-    
-      // Validate characterName using the same validations as in the model
+    const { characterName } = req.body;
       if (!validator.isLength(characterName, { min: 1 })) {
         return res.status(400).json({ error: 'Character name is required' });
       }
       if (!validator.isLength(characterName, { max: 20 })) {
         return res.status(400).json({ error: 'Name must be 20 characters or less' });
       }
-    
       try {
         const character = await Characters.findOneAndUpdate({ _id: id }, req.body, { new: true });
         if (!character) {
           return res.status(400).json({ error: 'No such character' });
         }
-    
         res.status(200).json(character);
       } catch (error) {
         res.status(500).json({ error: error.message });
--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -8,13 +8,11 @@ const createToken = (_id) => {
 // login a user
 const loginUser = async (req, res) => {
     const {email, password} = req.body
-
     try {
     const user = await User.login(email, password)
 
     // create a token
-    const token = createToken(user._id)
-
+const token = createToken(user._id)
     res.status(200).json({email, token})
     } catch (error) {
     res.status(400).json({error: error.message})
@@ -24,12 +22,11 @@ const loginUser = async (req, res) => {
 // signup a user
 const signupUser = async (req, res) => {
     const {name, email, password} = req.body
-
     try {
     const user = await User.signup(name, email, password)
 
-    // create a token
-    const token = createToken(user._id)
+// create a token
+const token = createToken(user._id)
 
     res.status(200).json({email, token})
     } catch (error) {
